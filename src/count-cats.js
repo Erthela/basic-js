76c36ee48e6cc8dbcc88138432a8460bf47a7e4f
Erthela <46851754+Erthela@users.noreply.github.com>
2020-11-01 22:55:09 +0300

    feat: solve task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    // throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+    let counter = 0;
+    matrix.forEach(key => key.forEach(key => {if (key == "^^") counter++}));
+    return counter;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    // throw new CustomError('Not implemented');
+    // remove line with error and write your code here
+    if (!Array.isArray(members)) return false;
+    let str = [];
+    members.forEach(key => {
+        if (typeof key === 'string') {
+            let newKey = key.trim();
+            str.push(newKey[0].toUpperCase());
+        }
+    });
+    return str.sort().join('');
 };
